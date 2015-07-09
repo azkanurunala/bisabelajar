28779120fc65966fbaecc0645398b8117dfcf5ed
Azka Nurun Ala <nurunalaazka@gmail.com>
2015-07-09 19:02:39 +0700

    a lot of changes

--- pages/about/layout/kata-mereka.php
+++ pages/about/layout/kata-mereka.php
@@ -1,107 +1,74 @@
-<section id="recent-info">
+<div id="kataMereka" class="tab-pane">
+<section>
     <div class="container">
         <div class="row text-center">
-            
-          <img class="icon" src="img/new_info.png"/>
-          <h2 class="section-heading">Info Terbaru</h2>
+
+          <h4 class="section-heading">Kata Mereka</h4>
           <hr class="primary">
                 
         </div>
 
         <div class="row">
             <div class="col-lg-12">
-                <div class="col-lg-6">
-                    <p>
-                        <a class="new-info title" href="#">
-                        <h4 class="new-info title"> <?php include ('contents/judul-1.txt'); ?></h4>
-                        </a>
-                        <a href="#">
-                        <img class="img-info-large" src="contents/gambar-1.png"/>
-                        </a>
-                        <h5 class="new-info author "><?php include ('contents/penulis-1.txt'); ?></h5>
-                        
+
+                        <p class="paragraph" >
+                          <img align="left" class="right-margin bottom-margin-narrow img-info-large" src="img/logo.png"/>
                        
-                        <p class="new-info paragraph" >
-                          <?php include('contents/isi-1.txt');?>
-
-                          <a class="link-info top-margin right-margin  text-right " href="#" >  BACA LEBIH LANJUT <span class="top-padding-narrow glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
+                          <?php include('contents/tentang/tentang-kami.txt');?>
                         </p>
-                    </p>               
-                </div>
+           
+             
 
-                <div class="top-margin  col-lg-6">
-                    
-                    <div class="bottom-border row">
-                        <div class="col-lg-5">
-                          
-                            <a href="#">
-                            <img class="img-info-large" src="contents/gambar-2.png"/>
-                            </a>
-                          
-                        </div>
-                        <div class="no-padding col-lg-7">
-                            <a href="#">
-                            <h4 class=" no-padding new-info title"> <?php include ('contents/judul-2.txt'); ?></h4>
-                            </a>
-                            <h5 class=" new-info author "><?php include ('contents/penulis-2.txt'); ?></h5>
-                            <p class="new-info paragraph" >
-                               <?php include('contents/isi-2.txt');?>
-                                <a class="link-info top-margin right-margin  text-right " href="#" >  BACA LEBIH LANJUT <span class="top-padding-narrow glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
-                            </p>
-                        </div>
-                     </div>
-
-                     <div class="bottom-border row">
-                        <div class="col-lg-5">
-                          
-                            <a href="#">
-                            <img class="img-info-large" src="contents/gambar-3.png"/>
-                            </a>
-                          
-                        </div>
-                        <div class="no-padding col-lg-7">
-                            <a href="#">
-                            <h4 class=" no-padding new-info title"> <?php include ('contents/judul-3.txt'); ?></h4>
-                            </a>
-                            <h5 class=" new-info author "><?php include ('contents/penulis-3.txt'); ?></h5>
-                            <p class="new-info paragraph" >
-                               <?php include('contents/isi-3.txt');?>
-                                <a class="link-info top-margin right-margin  text-right " href="#" >  BACA LEBIH LANJUT <span class="top-padding-narrow glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
-                            </p>
-                        </div>
-                     </div>
-
-                     <div class="bottom-border row">
-                        <div class="col-lg-5">
-                          
-                            <a href="#">
-                            <img class="img-info-large" src="contents/gambar-4.png"/>
-                            </a>
-                          
-                        </div>
-                        <div class="no-padding col-lg-7">
-                            <a href="#">
-                            <h4 class=" no-padding new-info title"> <?php include ('contents/judul-4.txt'); ?></h4>
-                            </a>
-                            <h5 class=" new-info author "><?php include ('contents/penulis-4.txt'); ?></h5>
-                            <p class="new-info paragraph" >
-                               <?php include('contents/isi-4.txt');?>
-                                <a class="link-info top-margin right-margin  text-right " href="#" > BACA LEBIH LANJUT <span class="top-padding-narrow glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
-                            </p>
-                        </div>
-                     </div>
-
-                     <div class="row text-right">
-                               <a class="link-info-full top-margin right-margin  text-right " href="#" > LIHAT SELENGKAPNYA <span class="top-padding-narrow glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h4> </a>
-                     </div>
-
-                </div>
                     
             </div>
 
         </div>
     </div> 
-       
-
    
 </section>
+
+<section id="valueKami" class="grey-bg">
+    <div class="container">
+        <div class="row text-center">
+
+
+
+          <div class="col-lg-12">
+                <div class="col-lg-3">
+                    <img class="value-icon" src="img/mm-icon.png">
+                    <h4 class="top-margin-narrow bottom-margin-narrow font-green bold">Materi Melimpah</h4>
+                    <p>Kami memiliki sumber materi pelajaran yang melimpah ruah</p>
+                </div>
+                <div class="col-lg-3">
+                    <img class="value-icon" src="img/vm-icon.png">
+                    <h4 class="top-margin-narrow bottom-margin-narrow font-green bold">Video Menarik</h4>
+                    <p>Dengan Konsep Video yang menarik, belajar pun lebih menyenangkan!</p>
+                </div>
+                <div class="col-lg-3">
+                    <img class="value-icon" src="img/ls-icon.png">
+                    <h4 class="top-margin-narrow bottom-margin-narrow font-green bold">Latihan Soal</h4>
+                    <p>Latihan soal di setiap bab membuat kamu semakin memahami materi pelajaran</p>
+                </div>
+                <div class="col-lg-3">
+                    <img class="value-icon" src="img/gr-icon.png">
+                    <h4 class="top-margin-narrow bottom-margin-narrow font-green bold">Gratis</h4>
+                    <p>Ya, kamu bisa mendapatkan semua itu secara cuma-cuma</p>
+                </div>
+          </div>
+                
+        </div>
+
+    </div> 
+   
+</section>
+
+<section id="compro">
+  <div class="container">
+    <div class="row text-center">
+        <iframe allow-fullscreen="true" width="720" height="360" src="https://www.youtube.com/embed/syBv25_aozo" frameborder="0" allowfullscreen>
+            
+        </iframe>
+    </div>
+   </div>
+</section>
+</div>
